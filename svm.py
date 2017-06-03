fc4e70f16b072e9da89451ccf19a152d87a978e9
nowshad-sust <iamnowshad@gmail.com>
2017-06-04 01:51:59 +0600

    tried svm for classification and success rate increased to 0.78

--- svm.py
+++ svm.py
@@ -4,9 +4,13 @@ import sklearn.datasets
 twenty_all = sklearn.datasets.load_files("./remail", 
 categories=None, load_content=True, shuffle=True, encoding="latin1", random_state=42, decode_error='strict')
 
+print('dataset loaded');
+
 # split the train and test data
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(twenty_all.data, twenty_all.target, test_size=0.2)
+
+print('data processing');
 
 # vectorize the training data
 from sklearn.feature_extraction.text import CountVectorizer
@@ -18,18 +22,26 @@ from sklearn.feature_extraction.text import TfidfTransformer
 tfidf_transformer = TfidfTransformer()
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
-# feed the train dataset into naive_bayes model
-from sklearn.naive_bayes import MultinomialNB
+print('training started');
+
+# feed training data into svm
+from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
 text_clf = Pipeline([('vect', CountVectorizer()),
                      ('tfidf', TfidfTransformer()),
-                     ('clf', MultinomialNB()),
+                     ('clf', SGDClassifier(loss='hinge', penalty='l2',
+                                           alpha=1e-3, n_iter=5, random_state=42)),
 ])
-text_clf = text_clf.fit(X_train, y_train)
+_ = text_clf.fit(X_train, y_train)
+
+print('Training complete');
+
+print('Testing trained model');
 
 # validation of the trained model
 import numpy as np
 docs_test = X_test
 predicted = text_clf.predict(docs_test)
 
+print('Test Result:');
 print(np.mean(predicted == y_test))
